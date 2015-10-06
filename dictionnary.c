83c3be8c653fc3ec8ed5dc804286647b339d7058
paul <paul@paul-W310CZ.(none)>
2015-10-06 15:58:24 +0200

    dictionnary.c

---
+++ dictionnary.c
@@ -0,0 +1,50 @@
+#include "libDictionnaire.h"
+
+
+
+
+Dictionnary create_dictionnary(){
+    return NULL;
+}
+
+Dictionnary add_word(Dictionnary, Word){
+
+
+}
+
+Dictionnary delete_word(Dictionnary, Word){
+
+
+
+}
+
+Boolean belongs_word(Dictionnary, Word){
+
+
+
+}
+
+Boolean void_dictionnary(Dictionnary){
+
+
+
+}
+
+void display_dictionnary(Dictionnary){
+
+
+
+}
+
+Dictionnary empty_dictionnary(Dictionnary){
+
+
+
+}
+
+Dictionnary save_dictionnary(Dictionnary){
+
+
+
+}
+
