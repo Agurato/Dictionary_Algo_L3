f805d3ff7171563f1caaa0d44867ca450f0119f3
paul <paul@paul-W310CZ.(none)>
2015-10-06 15:51:36 +0200

    libDictionnaire.h

--- libDictionnaire.h
+++ libDictionnaire.h
@@ -1,5 +1,5 @@
 typedef char* Word;
-typedef enum('Faux', 'Vrai') Boolean;
+typedef enum('False', 'True') Boolean;
 typedef struct noeud{
     char car;
     struct noeud *rightBrother, *leftSon;
@@ -8,4 +8,9 @@ typedef struct noeud{
 Dictionnary create_dictionnary();
 Dictionnary add_word(Dictionnary, Word);
 Dictionnary delete_word(Dictionnary, Word);
-Boolean belongs_word();
+Boolean belongs_word(Dictionnary, Word);
+Boolean void_dictionnary(Dictionnary);
+void display_dictionnary(Dictionnary);
+Dictionnary empty_dictionnary(Dictionnary);
+Dictionnary save_dictionnary(Dictionnary);
+Dictionnary load_dictionnary();
