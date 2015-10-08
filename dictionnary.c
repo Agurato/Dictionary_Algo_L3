c46193be33a6bae37de4ee04a0c986c3d73fd1b1
paul <paul@paul-W310CZ.(none)>
2015-10-08 09:07:43 +0200

    dictionnary.c and name of libDictionnaire change

--- dictionnary.c
+++ dictionnary.c
@@ -7,27 +7,40 @@ Dictionnary create_dictionnary(){
     return NULL;
 }
 
+/*Add a word in the dictionnary if he doesn't exist*/
 Dictionnary add_word(Dictionnary dictionnary, Word word){
 /*Chercher dans un premier temps où il faut mettre le mot puis ajouter*/
+    if(belongs_dictionnary(word)){
+        write("0","The word already exist in the dictionnary",45);
+    }
+    else{
 
+
+    }
+    return dictionnary;
 }
+
 /* Search the word with belongs_word and delete the Word*/
 Dictionnary delete_word(Dictionnary dictionnary, Word word){
-
+/* Search the word
+ * Delete it
+ * return the new dictionnary
+ */
 
 }
 
+/*If the word word belongs to the dictionnary return True else return False*/
 Boolean belongs_word(Dictionnary dictionnary, Word word){
 
 
 
 }
-/*Display words that are in the dictionnary*/
+
+/*Is the dictionnary empty or not ?*/
 Boolean void_dictionnary(Dictionnary dictionnary){
-
-
-
+    return (dictionnary = NULL);
 }
+
 /*Display words that are in the dictionnary*/
 void display_dictionnary(Dictionnary dictionnary){
 
---
+++ libDictionnary.h
@@ -0,0 +1,16 @@
+typedef char* Word;
+typedef enum('False', 'True') Boolean;
+typedef struct noeud{
+    char car;
+    struct noeud *rightBrother, *leftSon;
+}*Dictionnary
+
+Dictionnary create_dictionnary();
+Dictionnary add_word(Dictionnary, Word);
+Dictionnary delete_word(Dictionnary, Word);
+Boolean belongs_word(Dictionnary, Word);
+Boolean void_dictionnary(Dictionnary);
+void display_dictionnary(Dictionnary);
+Dictionnary empty_dictionnary(Dictionnary);
+int save_dictionnary(Dictionnary);
+Dictionnary load_dictionnary();
