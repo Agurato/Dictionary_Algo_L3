8ae6a28a0b930e3858fe2b02fde32f73ce67db53
paul <paul@paul-W310CZ.(none)>
2015-10-08 08:33:25 +0200

    dictionnary.c

--- dictionnary.c
+++ dictionnary.c
@@ -1,5 +1,5 @@
 #include "libDictionnaire.h"
-
+#include <malloc.h>
 
 
 
@@ -7,42 +7,41 @@ Dictionnary create_dictionnary(){
     return NULL;
 }
 
-Dictionnary add_word(Dictionnary, Word){
+Dictionnary add_word(Dictionnary dictionnary, Word word){
+/*Chercher dans un premier temps où il faut mettre le mot puis ajouter*/
+
+}
+/* Search the word with belongs_word and delete the Word*/
+Dictionnary delete_word(Dictionnary dictionnary, Word word){
 
 
 }
 
-Dictionnary delete_word(Dictionnary, Word){
+Boolean belongs_word(Dictionnary dictionnary, Word word){
 
 
 
 }
-
-Boolean belongs_word(Dictionnary, Word){
-
-
-
-}
-
-Boolean void_dictionnary(Dictionnary){
+/*Display words that are in the dictionnary*/
+Boolean void_dictionnary(Dictionnary dictionnary){
 
 
 
 }
-
-void display_dictionnary(Dictionnary){
-
-
-
-}
-
-Dictionnary empty_dictionnary(Dictionnary){
+/*Display words that are in the dictionnary*/
+void display_dictionnary(Dictionnary dictionnary){
 
 
 
 }
 
-Dictionnary save_dictionnary(Dictionnary){
+Dictionnary empty_dictionnary(Dictionnary dictionnary){
+
+
+
+}
+
+Dictionnary save_dictionnary(Dictionnary dictionnary){
 
 
 
