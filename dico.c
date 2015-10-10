22b05140cff81a0c5a2052b2c93b97cfffed3ff2
Agurato <vincent1611@hotmail.fr>
2015-10-10 19:40:58 +0200

    [add] libDico.h & dico.c for my functions to avoir troubles

---
+++ dico.c
@@ -0,0 +1,6 @@
+/* Ce sont mes essais de fonctions, stp n'y touche pas 
+C'est un fichier à part pour ne pas aoir à travailler à 2 sur le mêm fichier et risquer de problème */
+#include <stdio.h>
+#include <stdlib.h>
+#include <libDico.h>
+
